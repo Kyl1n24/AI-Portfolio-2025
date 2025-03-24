d1544acd1b167848405ed8951e87f1f2ae5a8239
Kyl1n24 <186019005+Kyl1n24@users.noreply.github.com>
2025-03-24 14:52:45 +1030

    Add February 17 practice files

---
+++ daily_progress/2025-02-17/practice_2025_02_17.py
@@ -0,0 +1,44 @@
+print(f"he is {30/10:.0f} years old.")
+
+print(f"""
+
+Most countries use the metric system for recipe measurement, but American bakers use a different system. For example, they use fluid ounces to measure liquids instead of milliliters (ml).
+
+So you need to convert recipe units to your local measuring system!
+
+For example, 8 fluid ounces of milk is {8*29.5735:.2f} ml. 
+And 100ml of water is {100 / 29.5735} fluid ounces.
+""")
+
+age = 28
+age = 5
+
+
+name = "Otto"
+gnome_height = 12.7
+print(f"Age: {age} ")
+print(f"Name: {name}")
+print(f"Gnome Height: {gnome_height}")
+
+score = 0
+print(score)
+
+score = score + 50
+print(score)
+
+score = score + 100
+print(score)
+
+score = score * 2
+print(score)
+
+print(f"Your final score was: {score}")
+
+dog_age = 49/7
+
+print(f""" {name}'s age in dog years is {dog_age}.""")
+
+
+
+
+
